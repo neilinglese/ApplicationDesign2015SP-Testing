3f2530530e0710dac1c64f8f1265532ea9c1ee08
Neil Inglese <neilinglese@gmail.com>
2015-03-18 16:47:51 -0500

    validation changes

--- test/validators-tests.js
+++ test/validators-tests.js
@@ -2,14 +2,41 @@ var assert = chai.assert;
 
 describe('validators', function () {
 	
-	describe('isString()', function () {
-        it('should return true when passed a string', function () {
-            assert.isTrue(validators.isString('osanetuha'));       
-        });   
-
-        it('should returnt false when passed a number', function () {
-            assert.isFalse(validators.isString(234));
-        });
-    });
-
+	// write some tests!
+		describe('.isString()', function () {
+			it('should be true if passed a string', function () {
+				assert.isTrue(validators.isString("words"));
+			});
+				it('should be false if not passed be a string', function () {
+				assert.isFalse(validators.isString(0));
+			});
+		});
+		
+		describe('.required()', function () {
+			it('should be true if passed a non null var', function () {
+				assert.isTrue(validators.required("words"));
+			});
+				it('should be false if not passed anything', function () {
+				assert.isFalse(validators.required());
+			});
+		});
+		
+		describe('.lengthAtLeast()', function () {
+			it('should be true if  string is greater then number passed', function () {
+				assert.isTrue(validators.lengthAtLeast("words",5));
+			});
+				it('should be false if not string is less then number passed', function () {
+				assert.isFalse(validators.lengthAtLeast("words",7));
+			});
+		});
+		
+		describe('.lengthAtMost()', function () {
+			it('should be true if  string is less then then number passed', function () {
+				assert.isTrue(validators.lengthAtMost("words",5));
+			});
+				it('should be false if not string is greater then number passed', function () {
+				assert.isFalse(validators.lengthAtMost("words",4));
+			});
+		});
+		
 });
