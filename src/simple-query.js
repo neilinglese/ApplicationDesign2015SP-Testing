cd7ead53613a72b1374fdbc48b18a6f27e9655ab
Neil Inglese <neilinglese@gmail.com>
2015-03-18 18:13:17 -0500

    css

--- src/simple-query.js
+++ src/simple-query.js
@@ -7,24 +7,32 @@
 
 	exports.simpleQuery = {
 		addClass: function(element, className) {
-			element.classList.add(className);
+			element.classList.add(className)
 		},
 		removeClass: function(element, className) {
-			element.classList.remove(className);
+			element.classList.remove(className)
 		},
 		toggleClass: function(element, className) {
-			element.classList.toggle(className);
+			element.classList.toggle(className)
 		},
 		css: function() {
 			// read about the arguments object in javascript, very handy....
 			// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Functions/arguments
+			//var args = Array.prototype.slice.call(arguments);
 			var element = arguments[0];
 			switch(arguments.length) {
-				case 2:
-					$(element).css(arguments[1]);
+				case 2: 
+				var styles = arguments[1];
+					for (var prop in styles)
+					{
+						console.log(prop);
+						element.style[prop] = styles[prop];
+					}
+					
+					
 					break;
 				case 3:
-					$(element).css(arguments[1], arguments[2]);
+					element.style[arguments[1]] = arguments[2];
 					break;
 				default:
 					throw 'simpleQuery.css() called with bad arguments';
